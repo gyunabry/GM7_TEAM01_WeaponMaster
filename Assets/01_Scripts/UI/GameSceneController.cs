e3e1afa50feafefd9a1700ad16079bc76c37b848
gyunabry <hanbin3471@gmail.com>
2026-06-24 11:27:36 +0900

    feat:Show gameover ui
    -PlayerController와 GameSceneController가 PlayerDeadEvent SO를 참조하도록 함.
    -플레이어의 체력이 0이 되면 이벤트를 발행하고, 이를 GameSceneController에서 구독해 이벤트 발행 시 게임오버 UI가 보여지도록 함

--- Assets/01_Scripts/UI/GameSceneController.cs
+++ Assets/01_Scripts/UI/GameSceneController.cs
@@ -1,21 +1,67 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameSceneController : MonoBehaviour
 {
+    [Header("플레이어 사망 이벤트")]
+    [SerializeField] private VoidEventChannel playerDeadEvent;
+
+    [Header("패널")]
+    [SerializeField] private CanvasGroup pauseCG;   // 일시정지 시 보여줄 패널
+    [SerializeField] private CanvasGroup optionCG;  // 일시정지 메뉴 중 옵션을 선택했을 때 보여줄 패널
+    [SerializeField] private CanvasGroup gameoverCG; // 게임오버 시 보여줄 패널
+
+    [Header("일시정지")]
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button restartButton;
+    [SerializeField] private Button optionButton;
+    [SerializeField] private Button exitButton;
+
+    [Header("게임오버")]
+    [SerializeField] private Button goRestartButton;
+    [SerializeField] private Button goTitleButton;
+
+    private void Start()
+    {
+        playerDeadEvent.OnEventRaised += OnPlayerDead;
+
+        if (resumeButton != null)
+            resumeButton.onClick.AddListener(() => OnClickResumeButton());
+        if (restartButton != null)
+            restartButton.onClick.AddListener(() => OnClickRestartButton());
+        if (optionButton != null)
+            optionButton.onClick.AddListener(() => OnClickOptionButton());
+        if (exitButton != null)
+            exitButton.onClick.AddListener(() => OnClickExitButton());
+
+        if (goRestartButton != null)
+            goRestartButton.onClick.AddListener(() => OnClickRestartButton());
+        if (goTitleButton != null)
+            goTitleButton.onClick.AddListener(() => OnClickExitButton());
+    }
+
+    private void OnDisable()
+    {
+        playerDeadEvent.OnEventRaised -= OnPlayerDead;
+    }
+
     #region 일시정지 메뉴
     public void OnClickResumeButton()
     {
-
+        GameManager.Instance.ResumeGame();
     }
 
     public void OnClickRestartButton()
     {
-
+        // 해당 난이도로 재시작
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void OnClickOptionButton()
     {
-
+        CanvasGroupController.EnableCG(optionCG);
+        CanvasGroupController.DisableCG(pauseCG);
     }
 
     public void OnClickExitButton()
@@ -24,4 +70,11 @@ public class GameSceneController : MonoBehaviour
         GameSceneManager.Instance.LoadScene(SceneType.Title);
     }
     #endregion
+
+    private void OnPlayerDead()
+    {
+        CanvasGroupController.EnableCG(gameoverCG);
+        CanvasGroupController.DisableCG(optionCG);
+        CanvasGroupController.DisableCG(pauseCG);
+    }
 }
