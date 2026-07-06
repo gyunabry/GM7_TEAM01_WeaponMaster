ac49409645929fab4b099601abbad5bc57cab3cf
Ritz55555 <rua6376@gmail.com>
2026-07-06 10:04:31 +0900

    Revert "feat:사운드, 윈도우 설정 저장"
    This reverts commit 9b7ddf55f790e149c659b2c6dab6fbe6c5808afb.

--- Assets/01_Scripts/UI/AllSetting.cs
+++ Assets/01_Scripts/UI/AllSetting.cs
@@ -1,10 +1,8 @@
 using NUnit.Framework;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.UI;
 
 public class resolutionsSetting : MonoBehaviour
 {
@@ -13,38 +11,6 @@ public class resolutionsSetting : MonoBehaviour
     private int nowWidth;
     private int nowHeight;
     [SerializeField] private AudioMixer audioMixer;
-
-    [SerializeField] private Slider MasterS;
-    [SerializeField] private Slider BGMS;
-    [SerializeField] private Slider SFXS;
-
-    enum ScreenFull {border = 0, full, window};
-    private void Awake()
-    {
-        LoadSet();
-    }
-    private void OnEnable()
-    {
-        float volMaster = PlayerPrefs.GetFloat("MasterV", 1.0f);
-        float volBGM = PlayerPrefs.GetFloat("BGMV", 1.0f);
-        float volSFX = PlayerPrefs.GetFloat("SFXV", 1.0f);
-        int isFull = PlayerPrefs.GetInt("FullScreen", 0);
-        MasterS.value = volMaster;
-        BGMS.value = volBGM;
-        SFXS.value = volSFX;
-        if(isFull == 0)
-        {
-            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, FullScreenMode.FullScreenWindow);
-        }
-        else if(isFull == 1)
-        {
-            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, FullScreenMode.ExclusiveFullScreen);
-        }
-        else if(isFull == 2)
-        {
-            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, FullScreenMode.Windowed);
-        }
-    }
     public void Start()
     {
         resol = new List<Resolution>();
@@ -80,14 +46,10 @@ public class resolutionsSetting : MonoBehaviour
     public void SetBorderless()
     {
         Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, FullScreenMode.FullScreenWindow);
-        PlayerPrefs.SetInt("FullScreen", (int)ScreenFull.border);
-        PlayerPrefs.Save();
     }
     public void SetFullScreen()
     {
         Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, FullScreenMode.ExclusiveFullScreen);
-        PlayerPrefs.SetInt("FullScreen", (int)ScreenFull.full);
-        PlayerPrefs.Save();
     }
     public void SetWindow()
     {
@@ -99,8 +61,6 @@ public class resolutionsSetting : MonoBehaviour
         {
             Screen.SetResolution(nowWidth, nowHeight, FullScreenMode.Windowed);
         }
-        PlayerPrefs.SetInt("FullScreen", (int)ScreenFull.window);
-        PlayerPrefs.Save();
     }
     public void SetMaster(float vo)
     {
@@ -114,8 +74,6 @@ public class resolutionsSetting : MonoBehaviour
             dbV = Mathf.Log10(vo) * 20;
         }
         audioMixer.SetFloat("Master", dbV);
-        PlayerPrefs.SetFloat("MasterV", vo);
-        PlayerPrefs.Save();
     }
     public void SetBGM(float vo)
     {
@@ -129,8 +87,6 @@ public class resolutionsSetting : MonoBehaviour
             dbV = Mathf.Log10(vo) * 20;
         }
         audioMixer.SetFloat("BGM", dbV);
-        PlayerPrefs.SetFloat("BGMV", vo);
-        PlayerPrefs.Save();
     }
     public void SetSFX(float vo)
     {
@@ -144,18 +100,5 @@ public class resolutionsSetting : MonoBehaviour
             dbV = Mathf.Log10(vo) * 20;
         }
         audioMixer.SetFloat("SFX", dbV);
-        PlayerPrefs.SetFloat("SFXV", vo);
-        PlayerPrefs.Save();
-    }
-    public void LoadSet()
-    {
-        float volMaster = PlayerPrefs.GetFloat("MasterV", 1.0f);
-        float volBGM = PlayerPrefs.GetFloat("BGMV", 1.0f);
-        float volSFX = PlayerPrefs.GetFloat("SFXV", 1.0f);
-        bool isFull = PlayerPrefs.GetInt("FullScreen", 1) == 1;
-
-        audioMixer.SetFloat("Master", volMaster);
-        audioMixer.SetFloat("BGM", volBGM);
-        audioMixer.SetFloat("SFX", volSFX);
     }
 }
